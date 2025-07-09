5b28fd36f4a94ee12878eb8cca1e39b7d62e9528
Jimin Byun <fraise3838@naver.com>
2025-07-09 23:11:23 +0900

    feat: 콘텐츠 모듈 JPA 엔티티 및 레포지토리 추가 (스키마 생성 목적)

--- src/main/java/com/popcoadmin/contents/repository/ActorRepository.java
+++ src/main/java/com/popcoadmin/contents/repository/ActorRepository.java
@@ -1,9 +1,9 @@
 package com.popcoadmin.contents.repository;
 
-import com.popcoadmin.contents.entity.Content;
+import com.popcoadmin.contents.entity.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContentRepository extends JpaRepository <Content, Long> {
+public interface ActorRepository extends JpaRepository<Actor, Long> {
 }
--- src/main/java/com/popcoadmin/contents/repository/DirectorRepository.java
+++ src/main/java/com/popcoadmin/contents/repository/DirectorRepository.java
@@ -1,9 +1,9 @@
 package com.popcoadmin.contents.repository;
 
-import com.popcoadmin.contents.entity.Content;
+import com.popcoadmin.contents.entity.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContentRepository extends JpaRepository <Content, Long> {
+public interface DirectorRepository extends JpaRepository<Director, Long> {
 }
